5ef160b8f4a8050ee1e0fc5e83e7813a6d0a84fb
Jaan-J <jaan.develop@gmail.com>
2022-09-24 18:29:12 -0400

    configure jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   env: {
-    browser: true,
     node: true,
     commonjs: true,
     es2021: true,
+    // Add this next line to configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
   },
   extends: 'eslint:recommended',
-  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 13,
   },
   rules: {},
 };
