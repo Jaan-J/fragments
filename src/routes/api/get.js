938035008d7e5825ad12e952e46f99cf1e27e4ce
Jaan-J <jaan.develop@gmail.com>
2022-11-14 16:22:20 -0500

    format file

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -31,29 +31,28 @@ module.exports = (req, res) => {
   }
 
   if (req.params.id && !req.query.expand) {
-
-      if (req.params.id.split('.').pop() === 'html'){
-        const paramID = (req.params.id.split('.')[0]);
-        try {
-          Fragment.byId(req.user, paramID).then((fragment) => {
-            var htmlRender = md.render(fragment.data.toString());
-            // if htmlrender ends with a newline, remove it
-            if (htmlRender.endsWith('\n')) {
-              htmlRender = htmlRender.slice(0, -1);
-            }
-            res.send(
-              createSuccessResponse({
-                'Content-Type': "text/html",
-                'Content-Length': fragment.size,
-                data: htmlRender
-              })
-            );
-          });
-        } catch (err) {
-          res.status(404).json(createErrorResponse(err));
-        }
-        return;
+    if (req.params.id.split('.').pop() === 'html') {
+      const paramID = req.params.id.split('.')[0];
+      try {
+        Fragment.byId(req.user, paramID).then((fragment) => {
+          var htmlRender = md.render(fragment.data.toString());
+          // if htmlrender ends with a newline, remove it
+          if (htmlRender.endsWith('\n')) {
+            htmlRender = htmlRender.slice(0, -1);
+          }
+          res.send(
+            createSuccessResponse({
+              'Content-Type': 'text/html',
+              'Content-Length': fragment.size,
+              data: htmlRender,
+            })
+          );
+        });
+      } catch (err) {
+        res.status(404).json(createErrorResponse(err));
       }
+      return;
+    }
 
     if (req.originalUrl.endsWith('/info')) {
       try {
